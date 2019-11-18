5c01cc9f986fe45c8fe71127a9115f2aa7167af0
ljm <ljm@ljmdeMacBook-Pro.local>
2019-11-18 23:08:01 +0800

    change

--- server.js
+++ server.js
@@ -8,38 +8,25 @@
 const path = require('path')
 const fs = require('fs')
 const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer()
-const createApp = require('./dist/bundle.server.js')
-const filePath = path.join(__dirname, './build/bundle.server.js')
+// const createApp = require('./dist/bundle.server.js')
+const filePath = path.join(__dirname, './dist/bundle.server.js')
 const code = fs.readFileSync(filePath, 'utf8')
-const bundleRenderer = vueServerRenderer.createBundleRenderer(code)
-// const creatApp = require('./src/app')
-
-// server.get('*', (req, res) => {
-//   const context = { url: req.url }
-//   const app = creatApp(context)
-//
-//   renderer.renderToString(app, (err, html) => {
-//       // 处理错误……
-//       res.end(html)
-//   })
-// })
+const renderer = require('vue-server-renderer').createBundleRenderer(code)
 
 server.get('*', (req, res) => {
-    const context = { url: req.url }
-    createApp(context).then(app => {
-        renderer.renderToString(app, (err, html) => {
-            if (err) {
-                if (err.code === 404) {
-                    res.status(404).end('Page not found')
-                } else {
-                    res.status(500).end('Internal Server Error')
-                }
-            } else {
-                res.end(html)
-            }
-        })
-    })
+  const context = { url: req.url }
+  renderer.renderToString((err, html) => {
+    if (err) {
+      if (err.code === 404) {
+        res.status(404).end('Page not found')
+      } else {
+        res.status(500).end('Internal Server Error')
+      }
+    } else {
+      console.log(html)
+      res.end(html)
+    }
+  })
 })
 
 server.listen(8080, () => {
